48281d5c42de7cd93202920d11333145813cb1b0
byteman <wangchen2011@gmail.com>
2018-05-27 21:23:02 +0800

    修改按键双击

--- projects/target_apps/ble_examples/ble_app_peripheral/src/key.c
+++ projects/target_apps/ble_examples/ble_app_peripheral/src/key.c
@@ -7,15 +7,19 @@
 #include "custs1_task.h"
 typedef struct {
 	 uint8 key;
-	 uint8 state; //1 pressed 0 relesed 2 long pressed
-	 int press_ts; //按下的时间计数起.
-	 int releas_ts; //释放的时间.
+		uint8 state; //1 pressed 0 relesed 2 long pressed
+		int press_stamp;
+		int press_ts; //按下的时间计数起.
+		int releas_ts; //释放的时间.
+		int relase_stamp; //上一次释放的时间戳.
+	
 	 GPIO_PORT port;
 	 GPIO_PIN  pin;
 }key_state;
 
 #define MAX_KEY_NUM 2
 #define KEY_PRESS_TIME 3
+#define KEY_ZERO_PRESS_TIME 5
 #define TIMER_INT 100
 #define PRESS_ALL_KEY_TIME 4000
 #define PRESS_ZERO_TIME 2000
@@ -23,7 +27,7 @@ typedef struct {
 #define ZERO_RLEASE_DELAY_NUM 20
 #define PRESS_TIME_TICK 3
 static key_state key_states[MAX_KEY_NUM];
-
+static int32 g_ts = 0;
 static uint8 key_pressed = 0;
 static key_event_func_t key_cb = 0;
 void  key_register(key_event_func_t cb)
@@ -59,7 +63,7 @@ void  key_isr()
 {
 		int i = 0;
 	 #if 1
-		
+		g_ts++;
 		if(GPIO_GetPinStatus(KEY_PWR_PORT,KEY_PWR_PIN) )
 		{
 			//被按下，开机后要延迟一段时间
@@ -110,8 +114,9 @@ void  key_isr()
 		if(GPIO_GetPinStatus(KEY_ZERO_PORT,KEY_ZERO_PIN))
 		{
 					key_states[1].press_ts++;
+					key_states[1].press_stamp++;
 					key_states[1].releas_ts=ZERO_RLEASE_DELAY_NUM;
-			
+					
 					if(key_states[1].press_ts > (PRESS_ZERO_TIME/TIMER_INT) )
 					{
 								if(GPIO_GetPinStatus(KEY_PWR_PORT,KEY_PWR_PIN))
@@ -133,13 +138,25 @@ void  key_isr()
 					{
 							send_key_msg(KEY_ZERO, KEY_PRESSED);
 					}
+			
+					
 		}
 		else
 		{
+					if(key_states[1].press_stamp > 0){
+									//再400ms内，按下了2次rlease就触发2次按键事件
+							if(abs(key_states[1].relase_stamp - g_ts) < 4){
+									send_key_msg(KEY_ZERO, KEY_RELASE_TWICE);
+									key_states[1].press_ts = 0;
+							}
+							key_states[1].relase_stamp = g_ts;
+							key_states[1].press_stamp = 0;
+					}
 					if(key_states[1].press_ts >= PRESS_TIME_TICK )
 					{
 							//曾经被按过，释放了
 							send_key_msg(KEY_ZERO, KEY_PRESS_RLEASED);
+							
 					}
 					//释放后需要等待2秒才发送按键被释放命令.
 					if(key_states[1].releas_ts > 0){
@@ -148,7 +165,7 @@ void  key_isr()
 					}
 					
 			//LOW LEVEL 按键被释放.
-					if(key_states[1].press_ts > KEY_PRESS_TIME && key_states[1].state != KEY_LONG_PRESSED)
+					if(key_states[1].press_ts > KEY_ZERO_PRESS_TIME && key_states[1].state != KEY_LONG_PRESSED)
 					{
 								
 								send_key_msg(KEY_ZERO, KEY_RELEASE_2S);
